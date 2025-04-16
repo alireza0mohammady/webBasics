7953a00e75dad5282480d6001c995c16e8a12b72
alireza0mohammadi <mr.afg00@gmail.com>
2025-04-16 23:15:25 +0330

    prototype and its inheritance

--- public/app-OOP.js
+++ public/app-OOP.js
@@ -1,43 +1,100 @@
-function StopWatch() {
-  let isStarted = false;
+// function StopWatch() {
+//   let isStarted = false;
+//
+//   let isStoped = false;
+//
+//   let duration = 0;
+//
+//   let startingTime = 0;
+//
+//   Object.defineProperty(this, "duration", {
+//     get() {
+//       if (!isStarted) return 0;
+//       if (isStarted && !isStoped) return Date.now() - startingTime + duration;
+//       return duration;
+//     },
+//   });
+//
+//   this.start = () => {
+//     if (isStarted && !isStoped) {
+//       console.error("timer is already started");
+//       return;
+//     }
+//     if (isStoped) {
+//       isStoped = false;
+//     }
+//     isStarted = true;
+//     startingTime = Date.now();
+//   };
+//   this.stop = () => {
+//     if (!isStarted || isStoped) {
+//       console.error("its not started yet");
+//       return;
+//     }
+//     isStoped = true;
+//     duration = Date.now() - startingTime + duration;
+//   };
+//   this.reset = () => {
+//     isStarted = false;
+//     isStoped = false;
+//     duration = 0;
+//     startingTime = 0;
+//   };
+// }
 
-  let isStoped = false;
+function HtmlElement() {
+  this.click = () => {
+    console.log("click");
+  };
+}
 
-  let duration = 0;
+HtmlElement.prototype.focus = () => {
+  console.log("focus");
+};
 
-  let startingTime = 0;
+let e = new HtmlElement();
 
-  Object.defineProperty(this, "duration", {
-    get() {
-      if (!isStarted) return 0;
-      if (isStarted && !isStoped) return Date.now() - startingTime + duration;
-      return duration;
+function HtmlSelectElement(elements = ["hello", "world"]) {
+  let storedItems = elements;
+
+  Object.defineProperty(this, "items", {
+    get: () => {
+      return storedItems;
     },
   });
 
-  this.start = () => {
-    if (isStarted && !isStoped) {
-      console.error("timer is already started");
-      return;
-    }
-    if (isStoped) {
-      isStoped = false;
-    }
-    isStarted = true;
-    startingTime = Date.now();
+  this.addElement = (element) => {
+    storedItems.push(element);
   };
-  this.stop = () => {
-    if (!isStarted || isStoped) {
-      console.error("its not started yet");
-      return;
-    }
-    isStoped = true;
-    duration = Date.now() - startingTime + duration;
+
+  this.removeElement = (element) => {
+    storedItems = storedItems.filter((e) => {
+      if (e !== element) return e;
+    });
   };
-  this.reset = () => {
-    isStarted = false;
-    isStoped = false;
-    duration = 0;
-    startingTime = 0;
+
+  this.render = () => {
+    let htmlOutPut = storedItems
+      .map((i) => {
+        return `<div>${i}</div>`;
+      })
+      .join("");
+    return htmlOutPut;
   };
 }
+
+HtmlSelectElement.prototype = new HtmlElement();
+HtmlSelectElement.prototype.constructor = HtmlSelectElement;
+
+function HtmlImgElement(src = "") {
+  this.src = src;
+  this.render = () => {
+    let imgHtml = `<img src=${src}>`;
+    return imgHtml;
+  };
+}
+
+HtmlImgElement.prototype = new HtmlElement();
+HtmlImgElement.prototype.constructor = HtmlImgElement;
+
+let ie = new HtmlImgElement("http what ever");
